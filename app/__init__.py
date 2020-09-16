c1273f4e32264024456e25691aa589a84936fb73
Dhany <dkovalsikoski@hotmail.com>
2020-09-16 01:08:28 -0400

    Primeiro commit, agora vai

---
+++ app/__init__.py
@@ -0,0 +1,10 @@
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/home')
+def home():
+    a = ['Semana ADS', 'Semana de Tecnologia', 'EIIFRO']
+    return render_template("index.html", atividades=a)
---
+++ run.py
@@ -0,0 +1,3 @@
+from app import app #para importar o módulo app
+
+If __name__ == "__main__": app.run(debug=True) #Pra rodar o método run do objeto app
